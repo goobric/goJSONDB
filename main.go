76e904fc46b965ba79aab45f8b6d0f8478ddea2d
goobric <goobric@gmail.com>
2023-09-27 18:47:22 +0800

    updated Delete func

--- main.go
+++ main.go
@@ -141,6 +141,21 @@ func (d *Driver) Delete(collection, resource string) error{
 	path := filepath.Join(collection, resource)
 	mutex := d.getOrCreateMutex(collection)
 	mutex.Lock()
+	defer mutex.Unlock()
+
+	dir := filepath.Join(d.dir, path)
+
+	switch fi, err := stat(dir);{
+	case fi == nil, err != nil:
+		return fmt.Errorf("unable to find file or dir named %v\n", path)
+
+	case fi.Mode().IsDir():
+		return os.RemoveAll(dir)
+
+	case fi.Mode().IsRegular():
+		return os.RemoveAll(dir + ".json")
+	}
+	return nil
 }
 
 func (d *Driver) getOrCreateMutex(collection string) *sync.Mutex{
